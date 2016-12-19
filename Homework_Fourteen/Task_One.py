01f2bffd004d78db8a32c29c5d1577ad142890b7
PanarinM <michaelpanarin@inbox.ru>
2016-12-20 00:13:56 +0200

    added generator + small usability tweaks

--- Homework_Fourteen/Task_One.py
+++ Homework_Fourteen/Task_One.py
@@ -1,32 +1,35 @@
-class Iterator(object):
+class Myiterator(object):
     def __init__(self, filename):
         self.filename = open(filename, 'r', encoding='UTF-8')
 
     def __iter__(self):
         return self
 
-    def next(self):
+    def __next__(self):
         for lines in self.filename:
-            return lines.strip()
+            return lines
         raise StopIteration
-
-    def close(self):
-        self.filename.close()
 
     def __del__(self):
         self.filename.close()
 
 
-if __name__ == '__main__':
-    a = Iterator('./trash/test')
+def Mygenerator(filename):
+    with open(filename, 'r', encoding='UTF-8') as a:
+        for lines in a:
+            yield lines
+
+
+def iterate_through(iter):
     try:
-        print(a.next())
-        print(a.next())
-        print(a.next())
-        print(a.next())
-        print(a.next())
-        print(a.next())
-        print(a.next())
+        while True:
+            print(next(iter).strip())
     except StopIteration:
         print('no more lines!')
 
+if __name__ == '__main__':
+    a = Myiterator('./trash/test')
+    iterate_through(a)
+
+    b = Mygenerator('./trash/test')
+    iterate_through(b)
